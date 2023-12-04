c4e599f5a93a2e4d029df6c46b8ba0acd20eb53c
Lemon03 <119447795+Lemon03@users.noreply.github.com>
2023-12-04 05:41:51 -0800

    q2.sh

--- q2.sh
+++ q2.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
 read POST_DATA
-decoded_data=$(echo "$POST_DATA" | sed 's/+/ /g;s/%\(..\)/\\x\1/g;')
 
-username=$(echo "$decoded_data" | grep -oE 'username=[^&]+' | cut -d '=' -f2)
-password=$(echo "$decoded_data" | grep -oE 'password=[^&]+' | cut -d '=' -f2)
-
-# Output headers
 echo "Content-type: text/plain"
 echo ""
-
-# Output username and password for debugging
-echo "Username: $username"
-echo "Password: $password"
+echo "POST Data: $POST_DATA"
