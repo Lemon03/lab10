f7d1b6e364a07c7184ccd038d2a624f70caf3098
Lemon03 <119447795+Lemon03@users.noreply.github.com>
2023-12-04 05:49:53 -0800

    q

--- q2.sh
+++ q2.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+# Read POST data
 POST_DATA=$(cat)
+
+# Extract boundary from CONTENT_TYPE environment variable
+BOUNDARY=$(echo $CONTENT_TYPE | sed 's/.*boundary=\(.*\)/\1/')
+
+# Process POST data to extract username and password
+username=$(echo "$POST_DATA" | awk -v RS="--$BOUNDARY" '/name="username"/ {getline; getline; print}' | tr -d '\r')
+password=$(echo "$POST_DATA" | awk -v RS="--$BOUNDARY" '/name="password"/ {getline; getline; print}' | tr -d '\r')
+
+# Output the parsed data
 echo "Content-type: text/plain"
 echo ""
-echo "POST Data: $POST_DATA"
+echo "Username: $username"
+echo "Password: $password"
