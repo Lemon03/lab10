6e6bf16f7a6ffd6cf9a08c1baa8ff06ae0f75067
Lemon03 <119447795+Lemon03@users.noreply.github.com>
2023-12-04 05:35:25 -0800

    q2.sh

--- q2.sh
+++ q2.sh
@@ -6,31 +6,10 @@ decoded_data=$(echo "$POST_DATA" | sed 's/+/ /g;s/%\(..\)/\\x\1/g;')
 username=$(echo "$decoded_data" | grep -oE 'username=[^&]+' | cut -d '=' -f2)
 password=$(echo "$decoded_data" | grep -oE 'password=[^&]+' | cut -d '=' -f2)
 
-if [ "$username" == "langara" ] && [ "$password" == "hello" ]; then
-    HEADER='{"alg":"HS256","typ":"JWT"}'
-    PAYLOAD="{\"username\":\"$username\",\"exp\":\"$(( $(date +%s) + 3600 ))\"}"
+# Output headers
+echo "Content-type: text/plain"
+echo ""
 
-    # Encode Header and Payload
-    B64_HEADER=$(echo -n "$HEADER" | base64 | tr '+' '-' | tr '/' '_' | tr -d '=')
-    B64_PAYLOAD=$(echo -n "$PAYLOAD" | base64 | tr '+' '-' | tr '/' '_' | tr -d '=')
-
-    # Signature
-    SIGNATURE=$(echo -n "${B64_HEADER}.${B64_PAYLOAD}" \
-        | openssl dgst -sha256 -hmac "your_secret_key" -binary \
-        | base64 | tr '+' '-' | tr '/' '_' | tr -d '=')
-
-    # Complete JWT
-    JWT="${B64_HEADER}.${B64_PAYLOAD}.${SIGNATURE}"
-
-    echo "Content-type: text/plain"
-    echo "Set-Cookie: JWT=$JWT; Path=/; HttpOnly"
-    echo ""
-    echo "Status: 200 OK"
-    echo "Login successful."
-else
-    echo "Content-type: text/plain"
-    echo ""
-    echo "Status: 401 Unauthorized"
-    echo "Invalid credentials."
-fi
-
+# Output username and password for debugging
+echo "Username: $username"
+echo "Password: $password"
